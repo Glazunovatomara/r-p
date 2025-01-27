f02b9c25ac7378394a904d0fc66b7b8eaa5caf12
Glazunova Tomara <glazunovatomara5@gmail.com>
2025-01-28 04:04:13 +0700

    feat:click on the button in the card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,30 @@
-import styles from './card.module.css'
+import styles from './card.module.css';
+import { useState } from 'react';
 
 const Card = () => {
+    const [state,setChange] = useState(0)
+
+    let btn = `${styles.btn}`
+    let translation = `${styles.invisibility}`
+
+    const handleClick = () =>{
+        setChange(state +1)
+    }
+
+    console.log (state)
+    if (state != 0) {
+        btn = `${styles.invisibility}`
+        translation = `${styles.translation}`
+    }
+
     return(
         <div className={styles.card}>
             <div className={styles.word_section}>
                 <h2 className={styles.subtitle}>слово</h2>
-                <p className={styles.transcription}>транскрипция</p>
+                <p className={styles.transcription}>[транскрипция]</p>
             </div>
-            <button className={styles.btn}>Проверить</button>
-            <p className={styles.translation}>перевод</p>    
+            <button className={btn} onClick={handleClick}>Проверить</button>
+            <p className={translation}>перевод</p>    
         </div>
     )
 }
